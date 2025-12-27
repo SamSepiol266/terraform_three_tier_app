1dc7ae1cdd2ce2e992fc12d6020a516243c0a052
Dana Bethel <0xsamsepiol@gmail.com>
2025-12-26 21:03:17 -0600

    Add AWS ALB and related resources in main.tf

---
+++ modules/alb/main.tf
@@ -0,0 +1,54 @@
+resource "aws_security_group" "alb_sg" {
+  name   = "${var.project_name}-alb-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    description = "Allow HTTP from Internet"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb" "alb" {
+  name               = "${var.project_name}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = var.public_subnets
+}
+
+resource "aws_lb_target_group" "tg" {
+  name     = "${var.project_name}-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
+
+resource "aws_lb_target_group_attachment" "tg_attach" {
+  count            = length(var.app_sg_id)
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = var.app_sg_id[count.index]
+  port             = 80
+}
+
+output "alb_dns_name" { value = aws_lb.alb.dns_name }
